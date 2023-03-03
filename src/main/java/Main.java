abd7daf2fb02d56a497727f2a559794d837e224a
Sarah Carson <sarah.kharaka@gmail.com>
2023-03-03 11:27:48 -0700

    fixed number check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,26 +7,31 @@ public class Main {
         int number1, number2=0, output;
         System.out.println("Welcome to the calculator.\nEnter a command:");
         Scanner input = new Scanner(System.in);
-        while (input.next() != "stop") {
-            command = input.next().substring(0);
+        String check = input.next();
+        while (!check.equals("stop")) {
+            command = check;
             number1 = input.nextInt();
-            if (input.hasNextInt()) {
-                number2 = input.nextInt();
-            }
+//            if (input.hasNext()) {
+//                number2 = input.nextInt();
+//            }
             switch (command) {
                 case "add":
+                    number2 = input.nextInt();
                     output = calculator.add(number1,number2);
                     System.out.println(output);
                     break;
                 case "subtract":
+                    number2 = input.nextInt();
                     output = calculator.subtract(number1,number2);
                     System.out.println(output);
                     break;
                 case "multiply":
+                    number2 = input.nextInt();
                     output = calculator.multiply(number1,number2);
                     System.out.println(output);
                     break;
                 case "divide":
+                    number2 = input.nextInt();
                     output = calculator.divide(number1,number2);
                     System.out.println(output);
                     break;
@@ -39,6 +44,7 @@ public class Main {
                     System.out.println(output);
                     break;
             }
+            check = input.next();
         }
     }
 }

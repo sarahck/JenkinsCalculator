1c4dff1a555d58ad209621180affd5bbebbc9b36
Sarah Carson <sarah.kharaka@gmail.com>
2023-03-03 10:17:32 -0700

    changed to public static

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
         String command;
         int number1, number2=0, output;
